d4e1460ace3ed466db49d3471f192d7281719aa8
Florian <duceptflorian53@gmail.com>
2023-05-30 17:05:12 +0200

    interface

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 
 exit = True
 print("Welcome to the Schedule App")
+print("The file must be an .xlsx or .csv file")
+print("The first task must be on the second row")
+print("The file must be as following : \nName        | Release Date| Due Date    | Processing Time\nTask Name   |01/01/2000   | 01/02/2000  | 15")
 fileName = input("Enter the name of the file : ")
 ext = fileName.split(".")[1]
 if os.path.isfile(fileName) and (ext == "csv" or ext == "xlsx"):
     exit = False
-    print("File Found....Reading your data")
+    print("File Found....Reading your datas")
     readFiles(fileName)
 else:
     print("File not found or does not have a .csv or .xlsx etension")
@@ -17,7 +20,17 @@ if not exit:
     # loop for the menu
     while not exit:
         print("\nMenu : \n1) View your data \n2) Modifiate your data \n3) Launch the analyse \n4) Exit...\n")
-        choice = int(input("Enter your choice : "))
+        while True:
+            try:
+                choice = int(
+                    input("Enter an integer between 1 and 4 :  "))
+                if 1 <= choice <= 4:
+                    break
+                else:
+                    print("The integer must be between 1 and 4")
+            except ValueError:
+                print("This is not an integer")
+
         if choice == 1:
             print_datas()
 
